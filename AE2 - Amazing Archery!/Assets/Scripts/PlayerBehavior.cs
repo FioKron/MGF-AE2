542e9412f1c32ecf55054fa658042697d7ce1d28
FioKron <jmor@hotmail.co.uk>
2016-11-14 21:36:58 +0000

    Amazing Archery! (baseline) 1.0.5
    Now with script for the 2nd 'user story' (as detailed in the latest version of the document, as of 21:36, on the 14/11/2016).

---
+++ AE2 - Amazing Archery!/Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    // The Phone's orientation in the real world (for detecting acceleration)
+    Quaternion PhoneOrientation;
+
+    // To see if there was any alteration from the previous orientation
+    Quaternion LastPhoneOrientation;
+
+	// Initilise this script:
+	void Start ()
+    {
+        // For checking in Update (to detect alterations):
+        if (SystemInfo.supportsGyroscope)
+        {
+            LastPhoneOrientation = Input.gyro.attitude;
+        }
+    }
+	
+	// Handle Updates in the game:
+	void Update ()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            PhoneOrientation = Input.gyro.attitude;
+        }
+
+        if (PhoneOrientation != LastPhoneOrientation)
+        {
+            AdjustAimPointForPhoneOrientation();
+        }
+    }
+
+    void AdjustAimPointForPhoneOrientation()
+    {
+        /**
+         * Only analyse rotation around the X and Z axes:
+        */
+
+        // Obtain the different for pitch first 
+        // (moving the Player if there is a difference):
+        float UpDownDirectionMagnitude = PhoneOrientation.x - LastPhoneOrientation.x;
+
+        if (UpDownDirectionMagnitude != 0.0f)
+        {
+            MoveCrosshairX(UpDownDirectionMagnitude);
+        }
+
+        // Then roll:
+        float LeftRightDirectionMagnitude = PhoneOrientation.z - LastPhoneOrientation.z;
+
+        if (LeftRightDirectionMagnitude != 0.0f)
+        {
+            MoveCrosshairZ(LeftRightDirectionMagnitude);
+        }
+    }
+
+    void MoveCrosshairX(float UpDownDirectionMagnitude)
+    {
+
+    }
+
+    void MoveCrosshairZ(float LeftRightDirectionMagnitude)
+    {
+
+    }
+}
