93d255916c88cb94bb0c51bd97db6f3288baca0f
FioKron <jmor@hotmail.co.uk>
2016-11-21 16:27:31 +0000

    Amazing Archery! 1.1.0
    The latest version of the Unity project and the documentation, as of 16:26 on the 21/11/2016.

--- AE2 - Amazing Archery!/Assets/Scripts/PlayerBehavior.cs
+++ AE2 - Amazing Archery!/Assets/Scripts/PlayerBehavior.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    // The Phone's orientation in the real world (for detecting acceleration)
-    Quaternion PhoneOrientation;
+    // The Phone's Acceleration in the real world (for detecting acceleration)
+    Vector3 PhoneAcceleration;
 
-    // To see if there was any alteration from the previous orientation
-    Quaternion LastPhoneOrientation;
+    // To see if there was any alteration from the previous Acceleration
+    Vector3 LastPhoneAcceleration;
+
+    // For altering the sensitivity of movement:
+    const float MOVEMENT_MULTIPLYER = 0.1f;
 
     // For debugging:
     float TestTimer = 0.0f;
@@ -17,15 +20,15 @@ public class PlayerBehavior : MonoBehaviour
     void Start ()
     {
         // You are required to explicitly enable the gyro-sensor (Nick would not know why either):
-        Input.gyro.enabled = true;
+        //Input.gyro.enabled = true;
 
         Screen.orientation = ScreenOrientation.Portrait;
 
         // For checking in Update (to detect alterations):
-        if (SystemInfo.supportsGyroscope)
+        if (SystemInfo.supportsAccelerometer)
         {
             //Message += "Initilisation Succesful";
-            LastPhoneOrientation = Input.gyro.attitude;
+            LastPhoneAcceleration = Input.acceleration;
         }
     }
 	
@@ -34,16 +37,19 @@ public class PlayerBehavior : MonoBehaviour
     {
         TestTimer += Time.deltaTime;
 
-        if (SystemInfo.supportsGyroscope)
+        if (SystemInfo.supportsAccelerometer)
         {
             //Message += "But this device supports a gyroscope...";
-            PhoneOrientation = Input.gyro.attitude;
+            PhoneAcceleration = Input.acceleration;
         }
 
-        if (PhoneOrientation != LastPhoneOrientation)
+        if (PhoneAcceleration != LastPhoneAcceleration)
         {
             //Message += "...time to adjust then...";
-            AdjustAimPointForPhoneOrientation();
+            //AdjustAimPointForPhoneAcceleration();
+
+            // Simply, one call to this function is all that is required, it seems: 
+            transform.Translate(Input.acceleration.x * MOVEMENT_MULTIPLYER, Input.acceleration.y * MOVEMENT_MULTIPLYER, 0.0f);
         }
 
         if (TestTimer >= 1000.0f)
@@ -53,14 +59,18 @@ public class PlayerBehavior : MonoBehaviour
         }
     }
 
-    void AdjustAimPointForPhoneOrientation()
+    /**
+    void AdjustAimPointForPhoneAcceleration()
     {
-        /**
-         * Only analyse rotation around the X and Z axes:
-        */
+        
+        Only analyse acceleration around the X and Z axes:
+        
+        float FinalLeftRightDirectionMagnitude = 0.0f;
+        float FinalUpDownDirectionMagnitude = 0.0f;
 
-        /**
-        // Obtain the different for pitch first 
+        
+        // VOID
+        Obtain the different for pitch first 
         // (moving the Player if there is a difference):
         
         Multiply to add or subtract a quaternion (a x b = c to add, Inverse(a) x b = c to subtract):
@@ -68,62 +78,71 @@ public class PlayerBehavior : MonoBehaviour
         //Quaternion a;
         //Quaternion b;
         //Quaternion c = Quaternion.Inverse(a) * b;
-        */
-        float UpDownDirectionMagnitude = PhoneOrientation.eulerAngles.x - LastPhoneOrientation.eulerAngles.x;
+        
 
-        //Message += PhoneOrientation;//transform.position;//"CurrentOrientation: " + PhoneOrientation + " LastOrientation: " + LastPhoneOrientation;
+        // Along Y first
+        // (Get the absolute value):
+        float UpDownDirectionMagnitude = Mathf.Abs(PhoneAcceleration.y - LastPhoneAcceleration.y);
+
+        //Message += PhoneAcceleration;//transform.position;//"CurrentAcceleration: " + PhoneAcceleration + " LastAcceleration: " + LastPhoneAcceleration;
 
         if (UpDownDirectionMagnitude != 0.0f)
         {
-            MoveCrosshairX(UpDownDirectionMagnitude);
+            FinalUpDownDirectionMagnitude = GetFinalUpDownDirectionMagnitude(UpDownDirectionMagnitude);
         }
 
-        // Then roll:
-        float LeftRightDirectionMagnitude = PhoneOrientation.eulerAngles.z - LastPhoneOrientation.eulerAngles.z;
+        // Then X
+        // (Get the absolute value):
+        float LeftRightDirectionMagnitude = Mathf.Abs(PhoneAcceleration.x - LastPhoneAcceleration.x);
 
         //Message += LeftRightDirectionMagnitude;
 
         if (LeftRightDirectionMagnitude != 0.0f)
         {
-            MoveCrosshairZ(LeftRightDirectionMagnitude);
+            FinalLeftRightDirectionMagnitude = GetFinalLeftRightDirectionMagnitude(LeftRightDirectionMagnitude);          
         }
+        //Message += "X: " + FinalLeftRightDirectionMagnitude + " Y: " + FinalUpDownDirectionMagnitude;
+        transform.Translate(FinalLeftRightDirectionMagnitude, FinalUpDownDirectionMagnitude, 0.0f);
     }
-
-    void MoveCrosshairX(float UpDownDirectionMagnitude)
-    {
-        // For a 'normalised' direction:
-        float FinalUpDownDirectionMagnitude = 0.0f;
-
-        if (UpDownDirectionMagnitude > 0.0f)
-        {
-            FinalUpDownDirectionMagnitude = 0.01f;
-        }
-        else if (UpDownDirectionMagnitude < 0.0f)
-        {
-            FinalUpDownDirectionMagnitude = -0.01f;
-        }
-
-        transform.Translate(0.0f, FinalUpDownDirectionMagnitude, 0.0f);
-    }
-
-    void MoveCrosshairZ(float LeftRightDirectionMagnitude)
+    
+    float GetFinalLeftRightDirectionMagnitude(float LeftRightDirectionMagnitude)
     {
         // For a 'normalised' direction:
         float FinalLeftRightDirectionMagnitude = 0.0f;
 
         if (LeftRightDirectionMagnitude > 0.0f)
         {
-            FinalLeftRightDirectionMagnitude = 0.01f;
+            FinalLeftRightDirectionMagnitude = LeftRightDirectionMagnitude * MOVEMENT_MULTIPLYER;
         }
         else if (LeftRightDirectionMagnitude < 0.0f)
         {
-            FinalLeftRightDirectionMagnitude = -0.01f;
+            FinalLeftRightDirectionMagnitude = -(LeftRightDirectionMagnitude) * MOVEMENT_MULTIPLYER;
+        }
+        Message += FinalLeftRightDirectionMagnitude;
+        return FinalLeftRightDirectionMagnitude;
+    }
+    
+    float GetFinalUpDownDirectionMagnitude(float UpDownDirectionMagnitude)
+    {
+        For a 'normalised' direction:
+        float FinalUpDownDirectionMagnitude = 0.0f;
+
+        if (UpDownDirectionMagnitude > 0.0f)
+        {
+            FinalUpDownDirectionMagnitude = UpDownDirectionMagnitude * MOVEMENT_MULTIPLYER;
+        }
+        else if (UpDownDirectionMagnitude < 0.0f)
+        {
+            FinalUpDownDirectionMagnitude = -(UpDownDirectionMagnitude) * MOVEMENT_MULTIPLYER;
         }
 
-        transform.Translate(FinalLeftRightDirectionMagnitude, 0.0f, 0.0f);
+        Message += FinalUpDownDirectionMagnitude;
+        return FinalUpDownDirectionMagnitude;
+        
     }
-
-    // For testing:
+    
+    
+    For testing:
     void OnGUI()
     {
         // In order to set the font and its respective size:
@@ -132,4 +151,5 @@ public class PlayerBehavior : MonoBehaviour
 
         GUI.Label(new Rect(100, 10, 150, 100), Message, DebugStyle);
     }
+    */
 }
